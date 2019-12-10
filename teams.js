534352bd296433b255c340d83c40bc6961b1c3c1
lieuraymond <lieu.raymond@gmail.com>
2019-12-09 19:44:46 -0800

    Update teams

--- teams.js
+++ teams.js
@@ -5,7 +5,7 @@ module.exports = function(){
 
     function getTeams(res, mysql, context, complete){
         //mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
-        mysql.pool.query("SELECT team_name FROM team", function(error, results, fields){
+        mysql.pool.query("SELECT team_name, team_color FROM team", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();

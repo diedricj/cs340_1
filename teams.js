60c50332f7b4fc3b3e354f6535e778b16124debb
Joshua Diedrich <diedricj@flip1.engr.oregonstate.edu>
2019-12-09 19:55:21 -0800

    teams delete

--- teams.js
+++ teams.js
@@ -5,7 +5,7 @@ module.exports = function(){
 
     function getTeams(res, mysql, context, complete){
         //mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
-        mysql.pool.query("SELECT team_name FROM team", function(error, results, fields){
+        mysql.pool.query("SELECT team_name, id FROM team", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -15,13 +15,29 @@ module.exports = function(){
         });
     }
 
+    router.delete('/:id', function(req, res){
+        console.log("I am in delete");
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM team WHERE id = " + req.params.id;
+        sql = mysql.pool.query(sql, function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
+
     /*Display all people. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
         var callbackCount = 0;
         console.log("trying here");
         var context = {};
-        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js", "deleteteams.js"];
         var mysql = req.app.get('mysql');
         getTeams(res, mysql, context, complete);
         function complete(){
